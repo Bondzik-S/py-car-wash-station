35cf7682643a2e228b9070d649fd040d2071ca7e
Bondzik-S <bondziks@gmail.com>
2024-10-01 22:06:15 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> None:
+    def serve_cars(self, cars: list[Car]) -> float:
         total_price = 0
 
         for car in cars:
